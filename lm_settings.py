8415a3803a89d4bae634d7b8cce863d69b671653
evatheunissen <eva.theunissen@student.vives.be>
2025-06-02 21:42:33 +0200

    update settings

--- lm_settings.py
+++ lm_settings.py
@@ -1,16 +1,14 @@
-from lm_sensor import Sensor, SensorArray
+from sensor import Sensor, SensorArray
 
 def load_SensorArray():
     print("Loading SensorArray...")
 
-    sensor_array = []
+    sa = SensorArray()
 
-    sensor_array.append(Sensor(14, 13, "FRONT")) # FRONT
-    sensor_array.append(Sensor(20, 21, "LEFT")) # LEFT
-    sensor_array.append(Sensor(10, 11, "MIDDLE")) # MIDDLE
-    sensor_array.append(Sensor(8, 7, "RIGHT")) # RIGHT
-    sensor_array.append(Sensor(3, 2, "BACK")) # BACK
-
-    sa = SensorArray(sensor_array)
+    sa.add(Sensor(14, 13)) # FRONT
+    sa.add(Sensor(20, 21)) # LEFT
+    sa.add(Sensor(10, 11)) # MIDDLE
+    sa.add(Sensor(8, 7)) # RIGHT
+    sa.add(Sensor(3, 2)) # BACK
 
     return sa
